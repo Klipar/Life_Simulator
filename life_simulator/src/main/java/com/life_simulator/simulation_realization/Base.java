762dc38a5e0cc4b4f5b9ec9299458e979ecf6c4a
klipar <bhdnkarpenko123@gmail.com>
2025-03-04 14:12:16 +0000

    Added a comparison method for the Base class

--- life_simulator/src/main/java/com/life_simulator/simulation_realization/Base.java
+++ life_simulator/src/main/java/com/life_simulator/simulation_realization/Base.java
@@ -30,4 +30,14 @@ public class Base {
         return "(" + X + ", " + Y + ")";
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Base myClass = (Base) obj;
+        return this.X == myClass.getX() && this.Y == myClass.getY();
+    }
+
+
 }
