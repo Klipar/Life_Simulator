62cbbbbd5d30e9c74e40cc778fc3cebddf656983
klipar <bhdnkarpenko123@gmail.com>
2025-03-05 22:26:21 +0000

    updated the way to interact with the getGridElement class

--- life_simulator/src/main/java/com/life_simulator/simulation_realization/GridElement.java
+++ life_simulator/src/main/java/com/life_simulator/simulation_realization/GridElement.java
@@ -5,17 +5,10 @@ import java.util.Map;
 
 public class GridElement extends Base{
     private Cell cell;
-
     private Map <String, Integer> factors = new HashMap<>();
 
     public GridElement(Base base){
         super(base.getX(), base.getY());
-
-        factors.put("LevelOfOrganicContamination", 0);
-        factors.put("Temperature", 0);
-        factors.put("LightingLevel", 0);
-        factors.put("AcidityLevel", 0);
-        factors.put("HumidityLevel", 0);
     }
 
     public boolean AddCell (Cell cell_inp){
@@ -47,43 +40,11 @@ public class GridElement extends Base{
         this.factors = factors;
     }
 
-    public int getLevelOfOrganicContamination() {
-        return factors.get("LevelOfOrganicContamination");
+    public int getFactor(String factor){
+        return factors.get(factor);
     }
 
-    public void setLevelOfOrganicContamination(int levelOfOrganicContamination) {
-        factors.put("LevelOfOrganicContamination", levelOfOrganicContamination);
-    }
-
-    public int getTemperature() {
-        return factors.get("Temperature");
-    }
-
-    public void setTemperature(int temperature) {
-        factors.put("Temperature", temperature);
-    }
-
-    public int getLightingLevel() {
-        return factors.get(("LightingLevel"));
-    }
-
-    public void setLightingLevel(int lightingLevel) {
-        factors.put("LightingLevel", lightingLevel);
-    }
-
-    public int getAcidityLevel() {
-        return factors.get("AcidityLevel");
-    }
-
-    public void setAcidityLevel(int acidityLevel) {
-        factors.put("AcidityLevel", acidityLevel);
-    }
-
-    public int getHumidityLevel() {
-        return factors.get("HumidityLevel");
-    }
-
-    public void setHumidityLevel(int humidityLevel) {
-        factors.put("humidityLevel", humidityLevel);
+    public void setFactor(String factor, int baseValue){
+        factors.put(factor, baseValue);
     }
 }
